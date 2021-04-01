c5f2f93597dec1610dc2be2e240e96703cefb513
George Antoniadis <george@noodles.gr>
2021-04-01 17:30:20 +0100

    feat: cleanup, add edit and delete messages

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
 	"net/http"
 	"sync"
@@ -28,6 +29,11 @@ var (
 		"assets/base.html",
 		"assets/frame.messages-new.html",
 	))
+	tplMessagesEdit = template.Must(template.ParseFS(
+		assets,
+		"assets/base.html",
+		"assets/frame.messages-edit.html",
+	))
 	tplMessageInner = template.Must(template.ParseFS(
 		assets,
 		"assets/inner.message.html",
@@ -37,11 +43,10 @@ var (
 type (
 	Room struct {
 		sync.RWMutex
-		handlers []chan<- *Message
 		Messages []*Message
 	}
 	Message struct {
-		ID      int64
+		ID      string
 		Body    string
 		Created time.Time
 	}
@@ -51,19 +56,50 @@ func (r *Room) AddMessage(body string) *Message {
 	r.Lock()
 	defer r.Unlock()
 	m := &Message{
-		ID:      time.Now().UnixNano(),
+		ID:      fmt.Sprintf("%d", time.Now().UnixNano()),
 		Body:    body,
 		Created: time.Now().UTC().Truncate(time.Second),
 	}
 	r.Messages = append(r.Messages, m)
-	for _, c := range r.handlers {
-		select {
-		case c <- m:
-		default:
-			// TODO delete c?
+	return m
+}
+
+func (r *Room) UpdateMessage(id, body string) (*Message, error) {
+	message, err := r.GetMessage(id)
+	if err != nil {
+		return nil, err
+	}
+	r.Lock()
+	defer r.Unlock()
+	message.Body = body
+	return message, nil
+}
+
+func (r *Room) GetMessage(id string) (*Message, error) {
+	r.Lock()
+	defer r.Unlock()
+	for _, m := range r.Messages {
+		if m.ID == id {
+			return m, nil
 		}
 	}
-	return m
+	return nil, fmt.Errorf("message not found")
+}
+
+func (r *Room) RemoveMessage(id string) {
+	r.Lock()
+	defer r.Unlock()
+	j := -1
+	for i, m := range r.Messages {
+		if m.ID == id {
+			j = i
+			break
+		}
+	}
+	if j < 0 {
+		return
+	}
+	r.Messages = append(r.Messages[:j], r.Messages[j+1:]...)
 }
 
 func (r *Room) ListMessages() []*Message {
@@ -72,19 +108,11 @@ func (r *Room) ListMessages() []*Message {
 	return r.Messages
 }
 
-func (r *Room) OnAddMessage() <-chan *Message {
-	r.Lock()
-	defer r.Unlock()
-	c := make(chan *Message)
-	r.handlers = append(r.handlers, c)
-	return c
-}
-
 func main() {
 	c := Room{
-		handlers: []chan<- *Message{},
-		RWMutex:  sync.RWMutex{},
+		RWMutex: sync.RWMutex{},
 		Messages: []*Message{{
+			ID:      fmt.Sprintf("%d", time.Now().UnixNano()),
 			Body:    "hello world",
 			Created: time.Now().UTC(),
 		}},
@@ -101,36 +129,71 @@ func main() {
 		)
 	})
 
-	r.Get("/messages", func(w http.ResponseWriter, r *http.Request) {
+	es := hotwire.NewEventStream()
+	r.Get("/events", es.ServeHTTP)
+
+	r.Get("/messages/new", func(w http.ResponseWriter, r *http.Request) {
 		tplMessagesNew.Execute(
 			w,
 			nil,
 		)
 	})
 
-	r.Post("/messages", func(w http.ResponseWriter, r *http.Request) {
+	r.Post("/messages/new", func(w http.ResponseWriter, r *http.Request) {
 		body := r.PostFormValue("body")
-		c.AddMessage(body)
+		message := c.AddMessage(body)
+		// send stream event to add the message
+		es.SendEvent(
+			hotwire.StreamActionAppend,
+			"room-messages",
+			tplMessageInner,
+			message,
+		)
 	})
 
-	es := hotwire.NewEventStream()
-	r.Get("/events", es.ServeHTTP)
-
-	go func() {
-		messages := c.OnAddMessage()
-		for {
-			message, ok := <-messages
-			if !ok {
-				return
-			}
-			es.SendEvent(
-				hotwire.StreamActionAppend,
-				"room-messages",
-				tplMessageInner,
-				message,
-			)
+	r.Get("/messages/edit", func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("id")
+		message, err := c.GetMessage(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
 		}
-	}()
+		tplMessagesEdit.Execute(
+			w,
+			message,
+		)
+	})
+
+	r.Post("/messages/edit", func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("id")
+		body := r.PostFormValue("body")
+		message, err := c.UpdateMessage(id, body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		// send stream event to update the message
+		es.SendEvent(
+			hotwire.StreamActionReplace,
+			"message-"+id,
+			tplMessageInner,
+			message,
+		)
+		// redirect to /messages/new
+		http.Redirect(w, r, "/messages/new", http.StatusFound)
+	})
+
+	r.Get("/messages/delete", func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("id")
+		c.RemoveMessage(id)
+		// send stream event to remove the message
+		es.SendEvent(
+			hotwire.StreamActionRemove,
+			"message-"+id,
+			tplMessageInner,
+			nil,
+		)
+	})
 
 	http.ListenAndServe(":3000", r)
 }
